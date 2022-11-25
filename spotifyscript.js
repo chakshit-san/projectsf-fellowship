78b712933a45a6c8d19b3d552fdcd580c7d85ceb
chakshit-san <projectcodel11@gmail.com>
2022-11-25 17:41:04 +0530

    some files of project is commited

---
+++ spotifyscript.js
@@ -0,0 +1,39 @@
+console.log("Welcome to Spotify")
+
+//initialize the variables
+let songIndex=0;
+let audioElement = new Audio('zaedenscna.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar= document.getElementById('myProgressBar');
+letgif= document.getElementById('gif');
+let songs = [
+    {songName:"Socha Na tha", filePath: "zaedenscna.mp3",coverPath: "sochana tha.jpg"},
+    {songName:"Socha Na tha", filePath: "zaedenscna.mp3",coverPath: "sochana tha.jpg"},
+    {songName:"Socha Na tha", filePath: "zaedenscna.mp3",coverPath: "sochana tha.jpg"},
+    {songName:"Socha Na tha", filePath: "zaedenscna.mp3",coverPath: "sochana tha.jpg"},
+    {songName:"Socha Na tha", filePath: "zaedenscna.mp3",coverPath: "sochana tha.jpg"},
+]
+
+// audioElement.play();
+// play the song 
+
+//handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+   else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
+})
+
+//listen to events
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    //update seekbar
+})
